8d91f5e2050566ceb84b85affc24caa0f577b2a5
BF667 <ericoardians@gmail.com>
2025-08-11 17:30:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -20,20 +20,20 @@ const moment= require("moment-timezone")
 
 const config = {
     AutoUpdate          : 'off', // on atau off
-    API_KEY             : '', // APIKEY ANDA AMBIL DI autoresbot.com
+    API_KEY             : '294767f1dd165a976f97016a', // APIKEY ANDA AMBIL DI autoresbot.com
     GEMINI_API_KEY      : '', // https://youtu.be/02oGg3-3a-s?si=9WhaVsLyfc6B-YYI
-    phone_number_bot    : '6285124002196', // Nomor BOT
+    phone_number_bot    : '6282135214781', // Nomor BOT
     type_connection     : 'pairing', // qr atau pairing
     bot_destination     : 'private', // group , private, both
-    name_bot            : 'Resbot Ai',
-    owner_name          : 'Autoresbot',
-    owner_number        : '6285246154386',
+    name_bot            : 'SDV Ai',
+    owner_name          : 'SDV Wa',
+    owner_number        : '6283150958207',
     owner_website       : 'autoresbot.com',
     version             : global.version,
     rate_limit          : 3000, // 3 detik
     total_limit         : 100, // limit perhari -  user biasa || kalo premium unlimited
-    sticker_packname    : 'Autoresbot',
-    sticker_author      : `Date: ${moment.tz('Asia/Jakarta').format('DD/MM/YY')}\nYouTube: Azhari Creative\nBot: 0852-4615-4386`,
+    sticker_packname    : 'SDV',
+    sticker_author      : `Serial Designation V`,
     notification        : {
         limit           : 'Hai kak, Limit harian anda sudah habis silakan tunggu besok ya atau berlangganan premium untuk menikmati fitur tanpa limit',
         reset           : 'Dialog berhasil dihapus. Semua percakapan kita telah di-reset dan siap memulai dari awal!',
